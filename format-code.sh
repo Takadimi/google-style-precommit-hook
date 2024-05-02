0243e59a71a731f0d7f5e9c32389566f3e85edc3
Takadimi <ethan.woodward@gmail.com>
2024-05-02 18:16:27 -0400

    make sure the jar is definitely not present

--- format-code.sh
+++ format-code.sh
@@ -4,6 +4,7 @@ cd .cache
 ls -l
 if [ ! -f google-java-format-1.7-all-deps.jar ]
 then
+    rm google-java-format-1.7-all-deps.jar
     curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
     chmod 755 google-java-format-1.7-all-deps.jar
 fi
