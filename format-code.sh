3ffc0eb36aa459fec8ede00bded8e22847f839e2
Takadimi <ethan.woodward@gmail.com>
2024-05-02 18:38:56 -0400

    clean up

--- format-code.sh
+++ format-code.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env sh
 mkdir -p .cache
 cd .cache
-ls -l
 if [ ! -f google-java-format-1.7-all-deps.jar ]
 then
-    # curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
-    # chmod 755 google-java-format-1.7-all-deps.jar
     wget "https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
     chmod 755 google-java-format-1.7-all-deps.jar
 fi
