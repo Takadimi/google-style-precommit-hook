beaf3e2b08cdf5652522e2d749e5efad70cdb3fd
Takadimi <ethan.woodward@gmail.com>
2024-05-02 18:08:53 -0400

    list out files in cache

--- format-code.sh
+++ format-code.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 mkdir -p .cache
 cd .cache
+ls -l
 if [ ! -f google-java-format-1.7-all-deps.jar ]
 then
     curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
