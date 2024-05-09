c24567fc2b118564c61aed1f28eb394e01782e61
Takadimi <ethan.woodward@gmail.com>
2024-05-09 17:33:36 -0400

    use specific output file name

--- format-code.sh
+++ format-code.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 mkdir -p .cache
 cd .cache
-if [ ! -f google-java-format-1.7-all-deps.jar ]
+if [ ! -f google-java-format.jar ]
 then
-    wget "https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
-    chmod 755 google-java-format-1.7-all-deps.jar
+    wget -O google-java-format.jar "https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
+    chmod 755 google-java-format.jar
 fi
 cd ..
 
 echo "Evaluating the following files: $@"
 
-java -jar .cache/google-java-format-1.7-all-deps.jar --replace $@
+java -jar .cache/google-java-format.jar --replace $@
